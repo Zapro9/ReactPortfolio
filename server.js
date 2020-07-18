6c410395df284773f004ee4dbc812e9e285b38f2
Zach Proctor <z.proctor@hotmail.com>
2020-07-17 23:56:46 -0600

    more testing

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
-const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || '8080';
+
+app.set("port", PORT)
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -16,8 +17,7 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pictureitdone");
+
 
 // Start the API server
 app.listen(PORT, function() {
